fe1dcb580f93bb32dfbd67405d952c202a68fa54
asifzafar00 <asizafar00@gmail.com>
2023-06-06 23:57:31 +0800

    Play button and Lazy column

--- app/src/main/java/com/example/navi/ui/activity/fragment/LazyColumnFragment.kt
+++ app/src/main/java/com/example/navi/ui/activity/fragment/LazyColumnFragment.kt
@@ -5,12 +5,22 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.compose.foundation.gestures.FlingBehavior
+import androidx.compose.foundation.gestures.ScrollableDefaults
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.lazy.LazyListScope
+import androidx.compose.foundation.lazy.LazyListState
+import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.ComposeView
+import androidx.compose.ui.unit.dp
 import androidx.fragment.app.Fragment
 import com.example.navi.obj.ObjectCompanions
 
@@ -50,8 +60,23 @@ class LazyColumnFragment : Fragment() {
                 }
             }
         }
-
     }
+
+    @Composable
+    fun LazyColumn(
+        modifier: Modifier = Modifier,
+        state: LazyListState = rememberLazyListState(),
+        contentPadding: PaddingValues = PaddingValues(0.dp),
+        reverseLayout: Boolean = false,
+        verticalArrangement: Arrangement.Vertical =
+            if (!reverseLayout) Arrangement.Top else Arrangement.Bottom,
+        horizontalAlignment: Alignment.Horizontal = Alignment.Start,
+        flingBehavior: FlingBehavior = ScrollableDefaults.flingBehavior(),
+        userScrollEnabled: Boolean = true,
+        content: LazyListScope.() -> Unit
+    ) {
+    }
+
 
     private fun MyUI() {
     }

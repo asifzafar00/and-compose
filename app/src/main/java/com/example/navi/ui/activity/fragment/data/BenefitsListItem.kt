18814b3d35ae63cf1862cfce4060a6830ed08833
asifzafar00 <asizafar00@gmail.com>
2023-06-21 14:44:16 +0800

    code clean up

--- app/src/main/java/com/example/navi/ui/activity/fragment/data/BenefitsListItem.kt
+++ app/src/main/java/com/example/navi/ui/activity/fragment/data/BenefitsListItem.kt
@@ -7,7 +7,6 @@ import androidx.compose.material.Card
 import androidx.compose.material.MaterialTheme.typography
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
